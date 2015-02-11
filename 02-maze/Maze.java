ca4bd2c2f00b69996c514fd92a02f30ba590adb8
Jason Shin <jasonshin98@gmail.com>
2015-02-11 08:00:58 -0500

    copied from classcode

--- 02-maze/Maze.java
+++ 02-maze/Maze.java
@@ -6,19 +6,28 @@ public class Maze
     private char[][] board;
     private int maxX;
     private int maxY;
-    private char wall = ' ';
-    private char exit = '$';
-    private char path = '#';
-    private char me = 'z';
-    private char visited = '.';
 
+    private char path='#';
+    private char wall=' ';
+    private char me='z';
+    private char exit='$';
+    private char visited = '.';
+    private boolean solved = false;
+		
+    public void delay(int n){
+	try {
+	    Thread.sleep(n);
+	} catch (Exception e) {}
+    }
+		
     public Maze() 
     {
 	maxX=40;
 	maxY=20;
 	board = new char[maxX][maxY];
-    	int i=0,j=0;
+				
 	try {
+						
 	    Scanner sc = new Scanner(new File("maze.dat"));
 	    int j=0;
 	    while (sc.hasNext())
@@ -34,48 +43,52 @@ public class Maze
 	catch (Exception e)
 	    {
 	    }
-    
+				
     }
 		
     public String toString()
     {
 	String s = "[2J\n";
+				
 	for (int y=0;y<maxY;y++)
 	    {
 		for (int x=0;x<maxX;x++)
 		    s = s +board[x][y];
 		s=s+"\n";
 	    }
-				
 	return s;
     }
 
-    public void solve(int x, int y) {
-	try {
-	    Thread.sleep(50);
-	} catch (Exception e) {}
-	if (board[x][y] == exit) {
-	    System.out.println(this);
-	    System.exit(0);
-	}
-	if (board[x][y] != path)
+    /*
+      solved - instance variable to indicate we're done
+			
+    */
+    public void solve(int x, int y){
+	if (board[x][y]==wall ||
+	    board[x][y]==me ||
+	    board[x][y]==visited ||
+	    solved){
 	    return;
+	}
 
-	board[x][y] = me;
+	if (board[x][y]==exit){
+	    System.out.println(this);
+	    solved = true;
+	}
+	delay(100);
 	System.out.println(this);
+	board[x][y]=me;
 	solve(x+1,y);
 	solve(x-1,y);
 	solve(x,y+1);
 	solve(x,y-1);
 	board[x][y]=visited;
     }
-	    
 		
     public static void main(String[] args){
 	Maze m = new Maze();
 	System.out.println(m);
-	System.out.println(m);
 	m.solve(1,1);
     }
-		
 }
+
