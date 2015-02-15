da7c10ddb10894f8354b97d1aa497dc7360e69f3
Jason Shin <jasonshin98@gmail.com>
2015-02-15 09:55:50 -0500

    Working on recursive step

--- 03-StateSpaceSearch/nqueens.java
+++ 03-StateSpaceSearch/nqueens.java
@@ -6,6 +6,10 @@ public class nqueens{
     private int size;
     private char queen = 'Q';
     private char bad = '.';
+    private char empty = 'X';
+    private int numQueen = 0;
+    private int numEmpty = 0;
+    private boolean solved = false;
 
     public void delay(int n){
 	try {
@@ -19,6 +23,7 @@ public class nqueens{
 	for (int i = 0; i < size; i++) {
 	    for (int n = 0; n < size; n++) {
 		chessboard[i][n] = 'X';
+		numEmpty++;
 	    }
 	}
     }
@@ -33,20 +38,81 @@ public class nqueens{
 		s = s + "\n";
 	    }
 	}
+	s += "\n";
 	return s;
     }
 
-    public void solve() {
+    public void solve(int x, int y) {
 	Random r = new Random();
-	chessboard[r.nextInt(size)][r.nextInt(size)] = queen;
-	
-	
+	//int[] badx = new int[](n*n);
+	//int[] bady = new int[](n*n);
+	char[][] origboard = chessboard;
+	int origNumEmpty = numEmpty;
+	int origNumQueen = numQueen;
+	if (numEmpty == 0 ||
+	    chessboard[x][y] != empty ||
+	    solved) {
+	    return;
+	}
+	if (numQueen == size) {
+	    System.out.println(this);
+	    solved = true;
+	}
+	chessboard[x][y] = queen;
+	numQueen++;
+	numEmpty--;
+	for (int i = 0; i < size; i++) {
+	    if (chessboard[x][i] == empty) {
+		chessboard[x][i] = bad;
+		numEmpty--;
+	    }
+	    if (chessboard[i][y] == empty) {
+		chessboard[i][y] = bad;
+		numEmpty--;
+	    }	  
+	    if (y - i - 1 >= 0 && x - i - 1 >= 0) {
+		if (chessboard[x - i - 1][y - i - 1] == empty) {
+		    chessboard[x - i - 1][y - i - 1] = bad;
+		    numEmpty--;
+		}
+	    }
+	    if (y + i + 1 < size && x + i + 1 < size) {
+		if (chessboard[x + i + 1][y + i + 1] == empty) {
+		    chessboard[x + i + 1][y + i + 1] = bad;
+		    numEmpty--;
+		}
+	    }
+	    if (y + i + 1 < size && x - i - 1 >= 0) {
+		if (chessboard[x - i - 1][y + i + 1] == empty) {
+		    chessboard[x - i - 1][y + i + 1] = bad;
+		    numEmpty--;
+		}
+	    }
+	    if (y - i - 1 >= 0 && x + i + 1 < size) {
+		if (chessboard[x + i + 1][y - i - 1] == empty) {
+		    chessboard[x + i + 1][y - i - 1] = bad;
+		    numEmpty--;
+		}
+	    }
+	}
 
+	delay(100);
+	System.out.println(this);
+        for (int i = 0; i < size; i++) {
+	    for (int n = 0; n < size; n++) {
+		if (numEmpty == 0) {
+		    chessboard = origboard;
+		    numQueen = origNumQueen;
+		    numEmpty = origNumEmpty;
+		}	
+		solve(i, n);	
+	    }
+	}
     }
 
     public static void main(String[] args) {
-	nqueens s = new nqueens(5);
-	System.out.println(s);
+	nqueens s = new nqueens(9);
+	s.solve(0, 2);
     }
 }
     
