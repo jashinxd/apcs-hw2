4da688278dfd09619c5931542328ec147ae5de09
Jason Shin <jasonshin98@gmail.com>
2015-02-23 08:41:44 -0500

    added makelake.java

---
+++ 04-usaco/makelake.java
@@ -0,0 +1,5 @@
+public class makelake {
+    private int[] pasture1 = {28,25,20,32,34,36};
+    private int[] pasture2 = {27,25,20,20,30,34};
+    private int[] pasture3 = {24,20,20,20,20,30};
+    private int[] pasture4 = [20,20,14,14,20,20};
