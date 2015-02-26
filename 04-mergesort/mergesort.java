e900f4d357d7d3bfea84e4c2aaf46a871e64e978
Jason Shin <jasonshin98@gmail.com>
2015-02-26 08:31:26 -0500

    finished merge, working on mergesort

--- 04-mergesort/mergesort.java
+++ 04-mergesort/mergesort.java
@@ -2,23 +2,61 @@ import java.io.*;
 import java.util.*;
 
 public class mergesort {
-    private Arraylist<Integer> deck = new Arraylist<Integer>;
-    private Arraylist<Integer> a = new Arraylist<Integer>;
-    private Arraylist<Integer> b = new Arraylist<Integer>;
+    private ArrayList<Integer> deck = new ArrayList<Integer>();
+    //private ArrayList<Integer> a = new ArrayList<Integer>();
+    //private ArrayList<Integer> b = new ArrayList<Integer>();
 
+    /*
     public mergesort() {
 	Random r = new Random();
 	for (int i = 0; i < 5; i++) {
-	    a.add(r.nextInt(10));
-	    b.add(r.nextInt(10));
+	    //a.add(r.nextInt(10));
+	    //b.add(r.nextInt(10));
+	    a.add(i);
+	    b.add(i);
 	}
     }
+    */
 
-    public Arraylist<Integer> merge(ArrayList<Integer> a, ArrayList<Integer> b){
-	
-	if (a.size <= 1) return a;
-	if (b.size <= 1) return b;
-	
+    public ArrayList<Integer> merge(ArrayList<Integer> a, ArrayList<Integer> b){
+	ArrayList<Integer> sorted = new ArrayList<Integer>();
+	while (a.size() > 0 || b.size() > 0) {
+	    if (b.size() == 0 && a.size() > 0) {
+		sorted.add(a.get(0));
+		a.remove(0);	       
+	    } else if (a.size() == 0 && b.size() > 0) {
+		sorted.add(b.get(0));
+		b.remove(0);		
+	    } else if (a.get(0) <= b.get(0)) {
+		sorted.add(a.get(0));
+		a.remove(0);
+	    } else if (b.get(0) <= a.get(0)) {
+		sorted.add(b.get(0));
+		b.remove(0);
+	    }
+	}
+	return sorted;
     }
+	    
+	    
+	//if (a.size <= 1) return a;
+	//if (b.size <= 1) return b;
+    public static void main(String[] args) {
+	mergesort m = new mergesort();
+	ArrayList<Integer> a = new ArrayList<Integer>();
+	ArrayList<Integer> b = new ArrayList<Integer>();
+	for (int i = 0; i < 5; i++) {
+	    a.add(i);
+	    b.add(i);
+	}
+	a.add(7);
+	a.add(9);
+	b.add(5);
+	b.add(6);
+	System.out.println(a);
+	System.out.println(b);
+	System.out.println(m.merge(a, b));	
+    }
+}
 
     
