2e9812e033d5a9327cf1ad4eaa173fb122d0b342
Jason Shin <jasonshin98@gmail.com>
2015-03-09 08:03:03 -0400

    Added selection

--- 05-selection/selection.java
+++ 05-selection/selection.java
@@ -12,6 +12,7 @@ public class selection {
 	return ret;
     }
 
+    /* -- Select by creating a new array
     public int select(int[] L, int SI, int EI) {
 	int[] D = new int[L.length];
 	for (int i = 0; i < L.length; i++) {
@@ -39,10 +40,51 @@ public class selection {
 	System.out.println(toString(D));
 	return SI;
     }
+    */
+
+    // --Select using in place
+    public int select(int[] A, int k, int low, int high) {
+	Random r = new Random();
+	int pivotind = r.nextInt(A.length);
+	int li = low;
+	int hi = high;
+	int swap;
+	int pivot = A[pivotind];
+	A[pivotind] = A[hi];
+	A[hi] = pivot;
+	System.out.println(pivotind);
+	System.out.println(toString(A));
+	while (li < hi) {
+	    if (A[li] < pivot) {
+	        li++;
+	    } else {
+		swap = A[li];
+		A[li] = A[hi];
+		A[hi] = swap;
+		hi--;
+	    }
+	    if (li == hi) {
+		li = pivotind;
+		A[high] = A[li];
+		A[li] = pivot;
+		li = hi;
+	    }
+	    System.out.println(toString(A));
+	}
+	System.out.println("Done");
+	if (pivotind == k) {
+	    return A[pivotind];
+	} else if (pivotind > k) {
+	    return select(A, k, low, hi);
+	} else {
+	    return select(A, k, li, high);
+	}
+    }
     
     public static void main(String[] args) {
 	selection s = new selection();
 	int[] ai = new int[]{7,2,8,3,9,6,1,0,5,4};
-	System.out.println(s.select(ai, 0, ai.length - 1));
+	System.out.println(s.select(ai, 4, 0, ai.length - 1));
+	//s.select(ai, 4, 0, ai.length - 1);
     }
 }
