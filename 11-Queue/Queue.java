16279bbee8d08f6220247ed1987c8b10ad8e6da9
Jason Shin <jasonshin98@gmail.com>
2015-03-26 08:30:25 -0400

    Finished Queue

--- 11-Queue/Queue.java
+++ 11-Queue/Queue.java
@@ -17,6 +17,42 @@ public class Queue<E> {
 	Node<E> temp = start.getNext();
 	start.setNext(d);
 	d.setNext(temp);
+	if (len == 1) {
+	    end = temp;
+	}
+    }
+
+    public E dequeue() {
+	/*
+	if (len==0) {
+	    return "Len = 0";
+	}
+	*/
+	E ans = start.getData();
+	if (len == 1) {
+	    ans= start.getNext().getData();
+	    start.setNext(end);
+	    len--;
+	}
+	else {
+	    for (Node<E> tmp = start.getNext(); tmp != null; tmp = tmp.getNext()) {
+		if (tmp.getNext().getNext() == null) {
+		    ans = tmp.getNext().getData();
+		    tmp.setNext(null);
+		    end = tmp;
+		    len--;
+		}
+	    }
+	}
+	return ans;
+    }
+    
+    public boolean empty() {
+	return len == 0;
+    }
+
+    public E head() {
+	return start.getNext().getData();
     }
 
     public String toString(){
@@ -36,5 +72,9 @@ public class Queue<E> {
 	q.enqueue(3);
 	q.enqueue(4);
 	System.out.println(q);
+	System.out.println(q.dequeue());
+	System.out.println(q);
+	System.out.println(q.empty());
+	System.out.println(q.head());
     }
 }
