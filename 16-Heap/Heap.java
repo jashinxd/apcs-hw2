e842a8172a1a1ab34e795a2ccda599df4f001e02
Jason Shin <jasonshin98@gmail.com>
2015-05-15 08:22:38 -0400

    .

---
+++ 16-Heap/Heap.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+
+public class Heap {
+    
+    private Node root;
+    private ArrayList<Integer> = new ArrayList<Integer>();
+    
+    public Heap() {
+	
+	
