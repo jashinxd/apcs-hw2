04dd3a0a9cd4c9eda8acc4de6b81976fe503fb8e
Jason Shin <jasonshin98@gmail.com>
2015-05-15 08:38:44 -0400

    heap

--- 16-Heap/Heap.java
+++ 16-Heap/Heap.java
@@ -4,8 +4,16 @@ import java.util.*;
 public class Heap {
     
     private Node root;
-    private ArrayList<Integer> = new ArrayList<Integer>();
+    private ArrayList<Integer> heap = new ArrayList<Integer>();
     
     public Heap() {
+	for (int i = 0; i < 10; i++) {
+	    Random r = new Random();
+	    heap.add(r.nextInt(10));
+	}
+    }
+
+    public 
+    
 	
 	
